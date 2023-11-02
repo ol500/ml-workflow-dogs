c4d5d3cd7f83c35707b6ae564c6eb8b3d23d9d72
oll500 <oll583@g.harvard.edu>
2023-11-02 13:11:00 +0100

    update docker-shell

--- ml-workflow-main/src/data-collector/docker-shell.sh
+++ ml-workflow-main/src/data-collector/docker-shell.sh
@@ -12,9 +12,9 @@ export GCP_PROJECT=spotted-399806
 export GCS_BUCKET_NAME="raw-spotted-data"
 
 # Build the image based on the Dockerfile
-#docker build -t $IMAGE_NAME -f Dockerfile .
+docker build -t $IMAGE_NAME -f Dockerfile .
 # M1/2 chip macs use this line
-docker build -t $IMAGE_NAME --platform=linux/arm64/v8 -f Dockerfile .
+#docker build -t $IMAGE_NAME --platform=linux/arm64/v8 -f Dockerfile .
 
 # Run Container
 docker run --rm --name $IMAGE_NAME -ti \
